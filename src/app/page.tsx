ea6be70616ab6cc7acbb31811d9bb1d0c0f9014e
japark <wkd5878@gmail.com>
2024-09-22 07:28:45 +0900

    feat - 메인페이지 스켈레톤 작업 (마크업)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import MainBody from "@/components/main/body";
+import {IoIosSearch, IoLogoJavascript} from "react-icons/io";
 
 export default function Home() {
   return (
     <>
-      <MainBody />
+        {/* Search Component */}
+        <form>
+            <input type="text"/>
+            <button><IoIosSearch /></button>
+        </form>
+
+        {/* Carousel */}
+        <div>
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+        </div>
+
+        {/* Item List Component */}
+        <div>
+            <div><IoLogoJavascript /></div>
+            <div>name</div>
+            <div>date</div>
+        </div>
     </>
   );
 }
