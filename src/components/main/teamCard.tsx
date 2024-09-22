447f294f144f621756267b7ea04da0c1d780bb77
japark <wkd5878@gmail.com>
2024-09-22 17:35:28 +0900

    feat - 글로벌 레이아웃 작업중

--- src/components/main/teamCard.tsx
+++ src/components/main/teamCard.tsx
@@ -3,9 +3,11 @@ import {IoLogoJavascript} from "react-icons/io";
 
 const TeamCard = () => {
     return (
-        <div className="flex">
-            <div><IoLogoJavascript/></div>
-            <div>name</div>
+        <div className="w-10/12 p-3 flex justify-between items-center">
+            <div className="flex items-center justify-center gap-2">
+                <div><IoLogoJavascript/></div>
+                <div>project name</div>
+            </div>
             <div>date</div>
         </div>
     );
