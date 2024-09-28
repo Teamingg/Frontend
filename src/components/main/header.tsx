7df92b2b2f857d225c880dac5ef39faf9a07a8ae
user <wkd5878@gmail.com>
2024-09-28 17:31:12 +0900

    refactoring - navbar item 배열로 관리 및 스타일 수정

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,12 +1,26 @@
 import styles from "@/css/main/header.module.css";
+import Link from "next/link";
+
+const navItems = [
+    {name: "팀원 구인", link: "/"},
+    {name: "멘토 구인", link: "/"},
+    {name: "로그인", link: "/"},
+]
 
 const Header = () => {
 
-    return(
-        <div className={styles.container}>
-            <div className={styles.title}>
+    return (
+        <div className="my-5 mx-3 flex justify-between">
+            <div>
                 <h2>Team 멍냥</h2>
             </div>
+            <ul className="flex">
+                {navItems.map((item, index) => (
+                    <li className="mx-5" key={index}>
+                        <Link href={item.link}>{item.name}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
