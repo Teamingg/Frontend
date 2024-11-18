b2350cdbf455b218fb5350055f795d7ab6932b10
Ajunmiiin <wjdals3249@naver.com>
2024-11-18 22:57:27 +0900

    사용자가 로그인 상태라면 로그아웃 버튼 렌더링 & 로그인하지 않은 상태라면 로그인 버튼이 보이도록 사용자의 로그인 상태를 확인하는 로직 추가

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,28 +1,35 @@
-import styles from "@/css/main/header.module.css";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
+import LogoutButton from '../LogoutButton';
 
-const navItems = [
-    {name: "팀원 구인", link: "/"},
-    {name: "멘토 구인", link: "/"},
-    {name: "로그인", link: "/"},
-]
+const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
+  return (
+    <div className="max-w-[1200px] mx-auto my-5 flex justify-between items-center">
+      {/* 로고 */}
+      <Link href="/">
+        <h1 className="relative w-[150px] h-[30px]">
+          <Image src="/Logo.png" fill alt="Teaming" sizes="150px" priority />
+        </h1>
+      </Link>
 
-const Header = () => {
-
-    return (
-        <div className="my-5 mx-3 flex justify-between">
-            <div>
-                <h2>Team 멍냥</h2>
-            </div>
-            <ul className="flex">
-                {navItems.map((item, index) => (
-                    <li className="mx-5" key={index}>
-                        <Link href={item.link}>{item.name}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+      {/* 네비게이션 */}
+      <ul className="flex gap-4">
+        <li>
+          {!isLoggedIn ? (
+            <Link href="/auth" scroll={false}>
+              로그인
+            </Link>
+          ) : (
+            <LogoutButton />
+          )}
+        </li>
+        <li>
+          <Link href="/my">마이페이지</Link>
+        </li>
+        <li>알림</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
