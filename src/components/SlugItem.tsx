a0fe37af34fd68391c47774a11975b07544de0d4
japark <wkd5878@gmail.com>
2024-11-26 10:27:56 +0900

    :sparkle: feat - 메인 페이지 내 콘텐츠 컴포넌트(SlugItem) 스켈레톤 작업 완료

---
+++ src/components/SlugItem.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SlugItem = () => {
+    return (
+        <div>
+            <div>
+                <p>
+                    <time dateTime="2024-11-11">2024.11.11</time>
+                    <span>~</span>
+                    <time dateTime="2025-02-11">2025.02.11</time>
+                </p>
+                <p>category</p>
+            </div>
+            <h3>title</h3>
+            <p>content</p>
+            <div>
+                <p>tag</p>
+                <p>team name</p>
+            </div>
+        </div>
+    );
+};
+
+export default SlugItem;
