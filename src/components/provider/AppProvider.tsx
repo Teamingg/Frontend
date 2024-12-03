7648efc5740f157066a974b989d4e05ccba3869e
Ajunmiiin <wjdals3249@naver.com>
2024-12-04 00:54:49 +0900

    fix : queryClient 재정의

--- src/components/provider/AppProvider.tsx
+++ src/components/provider/AppProvider.tsx
@@ -1,10 +1,12 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { ReactNode } from 'react';
+import { getQueryClient } from "@/shared/utils/Provider/getQueryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import React, { ReactNode } from "react";
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
