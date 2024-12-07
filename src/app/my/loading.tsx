7358a19160eb588193f9e1955bc97e83d2a60d33
Ajunmiiin <wjdals3249@naver.com>
2024-12-07 18:56:11 +0900

    Feat : 마이페이지 Loading.tsx 추가

---
+++ src/app/my/loading.tsx
@@ -0,0 +1,11 @@
+import LoadingIndicator from "@/shared/ui/LoadingIndicator";
+
+const Loading = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <LoadingIndicator />;
+    </div>
+  );
+};
+
+export default Loading;
