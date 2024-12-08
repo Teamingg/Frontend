56f45eed1b32495361207ccd3b6e0e0246d8e664
Ajunmiiin <wjdals3249@naver.com>
2024-12-08 14:36:36 +0900

    Design : 메인페이지 SectionHeader padding,border-radius 값 변경

--- src/components/SectioHeader.tsx
+++ src/components/SectioHeader.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
 
 type sectioHeaderProps = {
-    title: string;
-}
+  title: string;
+};
 
-const SectioHeader = ({title}: sectioHeaderProps) => {
-    return (
-        <header className="px-5 h-[55px] flex justify-between items-center bg-blue-500 rounded-3xl text-white">
-            <h3>{title}</h3>
-            <div><Link href="/">전체보기</Link></div>
-        </header>
-    );
+const SectioHeader = ({ title }: sectioHeaderProps) => {
+  return (
+    <header className="px-5 py-3 mb-4 flex justify-between items-center bg-blue-500 rounded-2xl text-white">
+      <h3>{title}</h3>
+      <div>
+        <Link href="/">전체보기</Link>
+      </div>
+    </header>
+  );
 };
 
 export default SectioHeader;
