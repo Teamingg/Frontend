360d4d0d6e11407bc3c0a77b75dddb8d52403c79
Ajunmiiin <wjdals3249@naver.com>
2024-12-09 20:36:08 +0900

    Feat : 프로젝트 페이지 추가

---
+++ src/app/(withNavigation)/project/page.tsx
@@ -0,0 +1,12 @@
+import SectionHeader from "@/components/SectionHeader";
+import React from "react";
+
+const ProjectPage = () => {
+  return (
+    <>
+      <SectionHeader title="팀 프로젝트" />
+    </>
+  );
+};
+
+export default ProjectPage;
