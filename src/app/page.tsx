d3a05cf1e80579e17317daddd208a1c656958112
user <wkd5878@gmail.com>
2024-12-13 23:24:38 +0900

    :art: refactoring - 메인페이지 너비 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,41 +5,38 @@ import SlugItem from "@/components/SlugItem";
 import SectioHeader from "@/components/SectioHeader";
 
 export default function Home() {
-    // const router = useRouter();
+  // const router = useRouter();
 
-    const OnClick = () => {
-        //  router.push("/login");
-    }
+  const OnClick = () => {
+    //  router.push("/login");
+  }
 
-    return (
-        // tailwindcss test
-        <>
-            <TeamProjectNavigation/>
+  return (
+    <div className="mx-16">
+      <TeamProjectNavigation/>
 
-            <div>
-                {/* project project */}
-                <div>
-                    <SectioHeader title="팀 프로젝트"/>
-                    <div className="p-5 grid grid-cols-2 gap-5">
-                        <SlugItem/>
-                        <SlugItem/>
-                        <SlugItem/>
-                        <SlugItem/>
-                    </div>
-                </div>
+      {/* project project */}
+      <div>
+        <SectioHeader title="팀 프로젝트"/>
+        <div className="p-5 grid grid-cols-2 gap-5">
+          <SlugItem/>
+          <SlugItem/>
+          <SlugItem/>
+          <SlugItem/>
+        </div>
+      </div>
 
-                {/* mentoring */}
-                <div className="mt-20">
-                    <SectioHeader title="멘토링"/>
-                    <div className="p-5 grid grid-cols-2 gap-5">
-                        <SlugItem/>
-                        <SlugItem/>
-                        <SlugItem/>
-                        <SlugItem/>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+      {/* mentoring */}
+      <div className="mt-20">
+        <SectioHeader title="멘토링"/>
+        <div className="p-5 grid grid-cols-2 gap-5">
+          <SlugItem/>
+          <SlugItem/>
+          <SlugItem/>
+          <SlugItem/>
+        </div>
+      </div>
+    </div>
+  );
 
 }
