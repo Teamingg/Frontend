a3f2227aaa3ae1cb21b1a5c4316d0152aaffefa1
Ajunmiiin <wjdals3249@naver.com>
2024-12-25 21:35:20 +0900

    feat : getMyMentoringTeam api 함수 추가 & useGetMyMentoringTeam 커스텀 훅 생성

---
+++ src/hooks/team/mentoring/useGetMyMentoringTeam.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+
+import MentoringTeam from "@/types/team/mentoring/mentoringTeam";
+import getMyMentoringTeam from "@/service/api/getMyMentoringTeam";
+
+const useGetMyMentoringTeam = () => {
+  const {
+    data: mentoringTeam,
+    error,
+    isError,
+  } = useQuery<MentoringTeam[]>({
+    queryKey: ["user", "team", "mentoring"],
+    queryFn: () => getMyMentoringTeam(),
+  });
+  return {
+    mentoringTeam,
+    error,
+    isError,
+  };
+};
+
+export default useGetMyMentoringTeam;
