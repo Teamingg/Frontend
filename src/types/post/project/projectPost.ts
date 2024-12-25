e9eac451ca7dd6205df3b1cd6147fc27ee01f677
Ajunmiiin <wjdals3249@naver.com>
2024-12-25 21:21:05 +0900

    feat : 프로젝트 POST 관련 인터페이스 추가

--- src/types/post/project/projectPost.ts
+++ src/types/post/project/projectPost.ts
@@ -1,3 +1,5 @@
+import { TeamStatus } from "@/types/team/teamStatus";
+
 export default interface ProjectPost {
   title: string; // 게시글 제목
   teamName: string; // 게시글을 작성한 팀 이름
@@ -7,9 +9,9 @@ export default interface ProjectPost {
   memberCnt: number; // 구인중인 멤버의 수
   link: string; // 연락방법
   contents: string; // 게시글 컨텐츠
-  postStatus: string; // 게시글 상태 ( 모집중, 모집완료 )
+  status: TeamStatus; // 게시글 상태 ( 모집중, 모집완료 )
   projectTeamId: number; // 게시글을 올린 프로젝트의 팀 ID
   postId: number; // 게시글 ID
-  stackIds: string[]; // 기술스택 리스트
+  stacks: string[]; // 기술스택 리스트
   recruitCategories: string[]; // 모집구분 리스트
 }
