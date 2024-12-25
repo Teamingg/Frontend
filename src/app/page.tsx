ab71ad417437472399287e292db47b8255fe4d3a
Ajunmiiin <wjdals3249@naver.com>
2024-12-25 21:29:21 +0900

    feat : 메인페이지에 프로젝트 게시물 & 멘토링 게시물 컴포넌트 렌더링 & 데이터를 prefetch하여 클라이언트 컴포넌트로 Hydration된 데이터를 전달하는 로직 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
-"use client";
-
 import "./globals.css";
 
-import TeamProjectNavigation from "@/components/layout/Layout/Header/GlobalNavigation";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { queryclient } from "@/lib/getQueryClient";
+import { getAllProjectPosts } from "@/service/api/getAllProjectPosts";
+import { getAllMentoringPosts } from "@/service/api/getAllMentoringPosts";
+
 import SectionHeader from "@/components/layout/Main/CatrgoryHeader";
-import SlugItem from "@/common/SlugItem";
+import TeamProjectNavigation from "@/components/layout/Layout/Header/GlobalNavigation";
 
-export default function Home() {
-  // const router = useRouter();
+import ProjectPostList from "@/components/post/ProjectPostList";
+import MentoringPostList from "@/components/post/MentoringPostList";
 
-  const OnClick = () => {
-    //  router.push("/login");
-  };
+export default async function Home() {
+  await queryclient.prefetchInfiniteQuery({
+    queryKey: ["project"],
+    queryFn: ({ pageParam }) =>
+      getAllProjectPosts({ nextCursor: pageParam as number }),
+    initialPageParam: 0,
+  });
+
+  await queryclient.prefetchInfiniteQuery({
+    queryKey: ["mentoring"],
+    queryFn: ({ pageParam }) =>
+      getAllMentoringPosts({ nextCursor: pageParam as number }),
+    initialPageParam: 0,
+  });
 
   return (
     // tailwindcss test
@@ -21,24 +35,20 @@ export default function Home() {
       <section className="flex flex-col gap-8 pb-8">
         {/* team project */}
         <div className="p-8 rounded-xl bg-[#f5f5f5]">
-          <SectionHeader title="팀 프로젝트" />
-          <div className="grid grid-cols-2 gap-5">
-            <SlugItem />
-            <SlugItem />
-            <SlugItem />
-            <SlugItem />
-          </div>
+          <SectionHeader title="팀 프로젝트" path="/project" />
+
+          <HydrationBoundary state={dehydrate(queryclient)}>
+            <ProjectPostList />
+          </HydrationBoundary>
         </div>
 
         {/* mentoring */}
         <div className="p-8 rounded-xl bg-[#f5f5f5]">
-          <SectionHeader title="멘토링" />
-          <div className="grid grid-cols-2 gap-5">
-            <SlugItem />
-            <SlugItem />
-            <SlugItem />
-            <SlugItem />
-          </div>
+          <SectionHeader title="멘토링" path="/mentoring" />
+
+          <HydrationBoundary state={dehydrate(queryclient)}>
+            <MentoringPostList />
+          </HydrationBoundary>
         </div>
       </section>
     </>
