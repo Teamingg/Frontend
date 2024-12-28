1d7e617adff392a15428fd75c2fb701f1c29f4d5
user <wkd5878@gmail.com>
2024-12-28 16:21:51 +0900

    :art: refactoring - 팀 페이지 경로 수정

--- src/app/team/layout.tsx
+++ src/app/team/layout.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import SectionLayout from "@/components/layout/DetailSection/SectionLayout";
 
-const Layout = () => {
+const teamPageLeaderPaths = [
+  {
+    label: "팀 소개",
+    path: "/team/1/leader/info",
+  },
+  {
+    label: "멤버 및 지원자 현황",
+    path: "/team/1/leader/member",
+  },
+  {
+    label: "작성한 게시글",
+    path: "/team/1/leader/post",
+  },
+];
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      
-    </div>
+    <SectionLayout sectionTitle="마이페이지" navPaths={teamPageLeaderPaths}>
+      {children}
+    </SectionLayout>
   );
 };
 
