8bd34e1ca9282e7a9ccfe3fb38756378c6c53cc1
Ajunmiiin <wjdals3249@naver.com>
2025-01-03 21:05:42 +0900

    fix : UserInfoSection Hydration 로직 삭제 & UserReviewList 컴포넌트 추가

--- src/app/my/(account)/page.tsx
+++ src/app/my/(account)/page.tsx
@@ -1,32 +1,18 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-
-import { queryclient } from "@/lib/getQueryClient";
-
-import { getUserInfo } from "@/service/api/user/getUserInfo";
-
 import UserInfoSection from "@/app/my/(account)/_components/UserInfoSection";
+import UserReviewList from "./_components/UserReview/UserReviewList";
 
-const AccountPage = async () => {
-  await queryclient.prefetchQuery({
-    queryKey: ["user"],
-    queryFn: getUserInfo,
-  });
-
+const AccountPage = () => {
   return (
     <div className="relative h-full">
       {/* 유저정보 (닉네임, 자기소개, 기술스택, 경고횟수) */}
-
-      <HydrationBoundary state={dehydrate(queryclient)}>
-        <UserInfoSection />
-      </HydrationBoundary>
+      <UserInfoSection />
 
       {/* 유저가 받은 평가 */}
-      <div className="bg-white rounded-lg p-6 shadow-sm">
-        <h2 className=" text-xl text-primary mb-4">내가 받은 후기</h2>
-        <p>
-          아직 팀원들에게 받은 후기가 없습니다. 프로젝트에 참여하고 후기를
-          받아보세요
-        </p>
+      <div>
+        <h2 className="bg-white rounded-lg shadow-sm p-4 text-primary text-xl mb-4">
+          내가 받은 후기
+        </h2>
+        <UserReviewList />
       </div>
 
       <button className="absolute bottom-0 right-0 bg-red-500 py-2 px-4 rounded-md text-white">
