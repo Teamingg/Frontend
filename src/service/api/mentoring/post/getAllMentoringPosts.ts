11e8d2690952b70c40787191801e682fa3ca14ca
Ajunmiiin <wjdals3249@naver.com>
2025-01-03 20:42:09 +0900

    feat : mentoring post 관련 API 추가

---
+++ src/service/api/mentoring/post/getAllMentoringPosts.ts
@@ -0,0 +1,19 @@
+import { instance } from "@/service/api/instance/axiosInstance";
+import MentoringPosts from "@/types/post/mentoring/mentoringPosts";
+
+export interface getAllMentoringPostsProps {
+  nextCursor?: number;
+}
+
+export const getAllMentoringPosts = async ({
+  nextCursor,
+}: getAllMentoringPostsProps): Promise<MentoringPosts> => {
+  const response = await instance.get(
+    nextCursor ? `/mentoring/posts?cursor=${nextCursor}` : "/mentoring/posts"
+    // nextCursor
+    //   ? `mock/mentoring/posts?cursor=${nextCursor}`
+    //   : "mock/mentoring/posts"
+  );
+
+  return response.data.data;
+};
