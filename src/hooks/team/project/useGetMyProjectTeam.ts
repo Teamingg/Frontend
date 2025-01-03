78da6fad414bfb619bab5d6d6b6cf0d5c6db1fc0
Ajunmiiin <wjdals3249@naver.com>
2025-01-03 20:56:05 +0900

    chore : getMyProjectTeam 경로 변경

--- src/hooks/team/project/useGetMyProjectTeam.ts
+++ src/hooks/team/project/useGetMyProjectTeam.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import ProjectTeam from "@/types/team/project/projectTeam";
 
-import getMyProjectTeam from "@/service/api/getMyProjectTeam";
+import getMyProjectTeam from "@/service/api/project/team/getMyProjectTeam";
 
 const useGetMyProjectTeam = () => {
   const {
