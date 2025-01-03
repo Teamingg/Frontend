5a45233fc2ea504fcc1f92290d3fe3939d58431a
Ajunmiiin <wjdals3249@naver.com>
2025-01-03 20:25:18 +0900

    chore : scrollbarHide 플러그인 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+import scrollbarHide from "tailwind-scrollbar-hide";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#337CEB',
+        primary: "#337CEB",
       },
     },
   },
-  plugins: [],
+  plugins: [scrollbarHide],
 };
