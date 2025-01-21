d087bafc5194d211872b37bd03f8927f89426d34
japark <wkd5878@gmail.com>
2025-01-21 10:05:24 +0900

    :art: chore - 멘토링 및 프로젝트 멤버 타입 정의 및 분리
    - CommonMember<T>: 공통 API 응답 타입을 제네릭으로 정의
    - MentoringTeamLeaderData: 멘토링 팀 리더 데이터 구조 정의
    - MentoringTeamMemberData: 멘토링 팀 멤버 데이터 구조 정의
    - ProjectMemberData: 프로젝트 멤버 데이터 구조 정의 및 배열 처리
    
    ✅ Export 타입
    - MentoringTeamLeader: 멘토링 팀 리더 응답 타입
    - MentoringTeamMember: 멘토링 팀 멤버 응답 타입
    - ProjectMember: 프로젝트 멤버 응답 타입

--- src/app/team/_type/teamPageMember.ts
+++ src/app/team/_type/teamPageMember.ts
@@ -1,10 +1,61 @@
-// 멘토링 멤버
-export interface MentoringMember {
-  // 스웨거 내 데이터 부재
+// 멤버의 상태 (MemberTables)
+export interface MemberStatus {
+  approved: boolean | null; // 수락 여부
+  removed: boolean; // 내보내기 여부
+  reported: boolean; // 신고 여부
+  written: boolean; // 작성 여부
+}
+
+// 공통 인터페이스
+interface CommonMember<T> {
+  status: number;
+  code: string;
+  message: string;
+  data: T;
+}
+
+// 리더용 반환데이터 구조
+interface MentoringTeamLeaderData {
+  teamId: number;
+  authority: "LEADER";
+  details: {
+    members: {
+      acceptedTime: string;
+      userId: number;
+      username: string;
+      role: "MENTOR" | "MENTEE";
+      status: "ACCEPTED" | "PENDING";
+      isLogined: boolean;
+      isDeleted: boolean;
+    }[];
+    participations: {
+      participatedTime: string;
+      userId: number;
+      username: string;
+      reportingCnt: number;
+      status: "ACCEPTED" | "PENDING";
+    }[];
+  };
+}
+
+
+// 멤버용 반환데이터 구조
+interface MentoringTeamMemberData {
+  teamId: number;
+  authority: "CREW";
+  details: {
+    acceptedTime: string;
+    userId: number;
+    username: string;
+    role: "MENTOR" | "MENTEE";
+    status: "ACCEPTED" | "PENDING";
+    isLogined: boolean;
+    isDeleted: boolean;
+  }[];
 }
 
 // 프로젝트 멤버
-export interface ProjectMember {
+export interface ProjectMemberData {
   "participationId": number,
   "userId": number,
   "projectTeamId": number,
@@ -20,10 +71,9 @@ export interface ProjectMember {
   "isReviewed": boolean
 }
 
-// 멤버의 상태 (MemberTables)
-export interface MemberStatus {
-  approved: boolean | null; // 수락 여부
-  removed: boolean; // 내보내기 여부
-  reported: boolean; // 신고 여부
-  written: boolean; // 작성 여부
-}
+// 멘토링 리더 / 멤버
+export type MentoringTeamLeader = CommonMember<MentoringTeamLeaderData>;
+export type MentoringTeamMember = CommonMember<MentoringTeamMemberData>;
+
+// 프로젝트 멤버
+export type ProjectMember = CommonMember<ProjectMemberData[]>
