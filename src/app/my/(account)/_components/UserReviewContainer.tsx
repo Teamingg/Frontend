143fae84ca35f7fec71d269ef07381f73759a61c
Ajunmiiin <wjdals3249@naver.com>
2025-01-24 18:08:39 +0900

    feat : 마이페이지 내 유저 리뷰 컨텐츠 prefetch 로직 추가 & HydrationBoundary 추가

---
+++ src/app/my/(account)/_components/UserReviewContainer.tsx
@@ -0,0 +1,19 @@
+import { queryclient } from "@/lib/getQueryClient";
+import { getUserReviews } from "@/service/api/user/getUserReviews";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import UserReviewList from "./UserReview/UserReviewList";
+
+const UserReviewContainer = async () => {
+  await queryclient.prefetchQuery({
+    queryKey: ["user", "review"],
+    queryFn: async () => await getUserReviews(),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryclient)}>
+      <UserReviewList />
+    </HydrationBoundary>
+  );
+};
+
+export default UserReviewContainer;
