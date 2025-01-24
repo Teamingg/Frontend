8b5e69b3a3c5fe1fde9f03e2ed783b6d0602f6f2
Ajunmiiin <wjdals3249@naver.com>
2025-01-24 18:15:16 +0900

    feat : useServer 함수로 변경, headers 값에 Cookie 넘겨주는 로직 작성

--- src/service/api/user/getUserInfo.ts
+++ src/service/api/user/getUserInfo.ts
@@ -1,9 +1,16 @@
+"use server";
+
 import { instance } from "@/service/api/instance/axiosInstance";
 
 import { UserInfo } from "@/types/user/userInfo";
+import { cookies } from "next/headers";
 
 export const getUserInfo = async (): Promise<UserInfo> => {
-  const response = await instance.get("/users");
+  const response = await instance.get("/users", {
+    headers: {
+      Cookie: cookies().toString(),
+    },
+  });
 
   if (response.status !== 200) {
     throw new Error("정보를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.");
