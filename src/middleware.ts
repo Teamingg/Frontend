2140ad7e72d082631bcec15baf7148f5b27c4e66
Ajunmiiin <wjdals3249@naver.com>
2025-01-31 20:28:02 +0900

    chore : cookie 관련 유틸 함수 경로 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { refreshToken as reissue } from "./service/api/refreshToken";
 
-import checkCookie from "./utils/auth/checkCookie";
-import getCookie from "./utils/auth/getCookie";
+import { checkCookie, getCookie } from "./utils/cookies";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
