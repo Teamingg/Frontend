2babd5adb46d0d0d8c98769cd430a4992de566be
JangHwan <wkd5878@gmail.com>
2025-02-09 17:01:21 +0900

    ✨ feat: UserInfoItem.tsx 컴포넌트 추가 및 UserInfoContent.tsx 코드 개선
    - UserInfoContent 내 반복되는 로직을 UserInfoItem 컴포넌트화
    - as 속성을 사용하여 HTML 태그를 동적으로 변경 가능하도록 설정 (div, article)

--- src/app/my/(account)/@info/(user-info)/_components/UserInfo/UserInfoContent.tsx
+++ src/app/my/(account)/@info/(user-info)/_components/UserInfo/UserInfoContent.tsx
@@ -1,4 +1,5 @@
 import SelectBoxField from "@/types/selectBoxField";
+import UserInfoItem from "@/app/my/(account)/@info/(user-info)/_components/ui/UserInfoItem";
 
 interface UserInfoContentProps {
   name: string;
@@ -14,43 +15,43 @@ const UserInfoContent = ({
   stacks,
 }: UserInfoContentProps) => {
   return (
-    <div>
-      <div className="flex mb-4">
-        <div className="w-1/2">
-          <p className="mr-4 text-lg font-semibold">닉네임</p>
-          <p>{name}</p>
-        </div>
-        <div className="w-1/2">
-          <p className="mr-4 text-lg font-semibold">경고횟수</p>
-          <p>{waringCnt > 0 ? waringCnt : "0"}</p>
-        </div>
-      </div>
-
-      <div className="mb-4">
-        <p className="text-lg font-semibold">자기소개</p>
-        <p className="text-black">
-          {introduce.length === 0
-            ? "아직 소개를 작성하지 않았습니다. 팀원들에게 나를 소개하는 글을 작성해보세요"
-            : introduce}
-        </p>
-      </div>
-
       <div>
-        <p className="text-lg font-semibold mb-2">기술스택</p>
-        <p>
+        <div className="flex mb-4">
+          <UserInfoItem
+              label="닉네임"
+              value={name}
+              className="w-1/2"
+              labelClassName="mr-4"/>
+          <UserInfoItem
+              label="경고횟수"
+              value={waringCnt > 0 ? String(waringCnt) : "0"}
+              className="w-1/2"
+              labelClassName="mr-4"/>
+        </div>
+        <UserInfoItem
+            label="자기소개"
+            as="article"
+            className="mb-4">
+          {introduce.length === 0
+              ? "아직 소개를 작성하지 않았습니다. 팀원들에게 나를 소개하는 글을 작성해보세요"
+              : introduce}
+        </UserInfoItem>
+
+        <UserInfoItem
+            label="기술스택"
+            as="article"
+            className="mb-4">
           {stacks.length === 0
-            ? "선택된 기술스택이 없습니다."
-            : stacks.map((stack) => (
-                <span
-                  className="border rounded-md 00 p-2 mr-2"
-                  key={stack.value}
-                >
+              ? "선택된 기술스택이 없습니다."
+              : stacks.map((stack) => (
+                  <span
+                      className="border rounded-md 00 p-2 mr-2"
+                      key={stack.value}>
                   {stack.label}
                 </span>
               ))}
-        </p>
+        </UserInfoItem>
       </div>
-    </div>
   );
 };
 
