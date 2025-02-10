60a0baf043ec51810b9654a37d4b97db801a7afc
Ajunmiiin <wjdals3249@naver.com>
2025-02-10 21:55:14 +0900

    feat : 다른 유저의 프로필 모달 페이지 생성

---
+++ src/app/@modal/(.)user/profile/[userId]/(team)/mentoring/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import MyTeamItem from "@/app/my/(team)/_components/MyTeamItem";
+import { useGetUserTeam } from "@/hooks/queries/user";
+
+import { useParams } from "next/navigation";
+import React from "react";
+
+const UserProfileMentoringTeamPage = () => {
+  const { userId } = useParams();
+
+  const { data: teamList } = useGetUserTeam({
+    category: "mentoring",
+    userId: userId as string,
+  });
+
+  if (!teamList) {
+    return null;
+  }
+
+  console.log(teamList);
+  return (
+    <>
+      <ul className="flex flex-col gap-4 pt-4 max-h-[590px] overflow-y-scroll scrollbar-hide">
+        {teamList.map((team) => (
+          <MyTeamItem
+            name={team.name}
+            key={team.id}
+            id={team.id}
+            status={team.status}
+            role={team.authority}
+            category={"mentoring"}
+            // replace={true}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UserProfileMentoringTeamPage;
