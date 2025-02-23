da8839dce1e142dd09ed7f89bc619557e909e4b3
JangHwan <wkd5878@gmail.com>
2025-02-23 19:20:20 +0900

    :recycle: refactor - SectionLayout.tsx 컴포넌트 수정
    - AsideNav(SectionTabNav.tsx)를 선택적으로 받을 수 있도록 수정
    - 기본값 false

--- src/components/layout/DetailSection/SectionLayout.tsx
+++ src/components/layout/DetailSection/SectionLayout.tsx
@@ -1,3 +1,4 @@
+import {ReactNode} from "react";
 import SectionTabNav from "./SectionTabNav";
 
 export interface navPath {
@@ -6,30 +7,36 @@ export interface navPath {
 }
 
 interface SectionLayoutProps {
-  children: React.ReactNode;
+  children: ReactNode;
   sectionTitle: string;
   navPaths: navPath[];
+  isAsideNav?: boolean;
 }
 
 const SectionLayout = ({
   children,
   sectionTitle,
   navPaths,
+  isAsideNav = false,
 }: SectionLayoutProps) => {
   return (
     <div className="w-full h-full md:max-w-[1400px] md:mx-auto">
       <div className="w-full h-full rounded-lg bg-white md:bg-[#f5f5f5] md:p-4">
         <div className="block md:flex gap-4 h-full w-full bg-[#f5f5f5]">
           {/* 좌측 ( 타이틀 & 네비게이션 ) */}
-          <section className="md:w-1/4 md:h-full md:py-4 bg-white md:bg-[#f5f5f5]">
-            <h1 className="text-center md:text-left text-lg md:text-2xl font-semibold md:mb-4 pt-4 md:pt-0 md:px-4">
-              {sectionTitle}
-            </h1>
-            <SectionTabNav navPaths={navPaths} />
-          </section>
+          {isAsideNav && (
+              <aside className="md:w-1/4 md:h-full md:py-4 bg-white md:bg-[#f5f5f5]">
+                <h1 className="text-center md:text-left text-lg md:text-2xl font-semibold md:mb-4 pt-4 md:pt-0 md:px-4">
+                  {sectionTitle}
+                </h1>
+                <SectionTabNav navPaths={navPaths} />
+              </aside>
+          )}
 
           {/* 우측 ( 섹션 컨텐츠 ) */}
-          <section className="w-full md:h-full">{children}</section>
+          <section className="w-full md:h-full">
+            {children}
+          </section>
         </div>
       </div>
     </div>
