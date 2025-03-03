26873525d3a0fce4305a6e28c53f0dd34dba79cc
JangHwan <wkd5878@gmail.com>
2025-03-03 22:57:09 +0900

    :art: chore - 컴포넌트 애니메이션 및 메모제이션 적용
    애니메이션
    - TeamHeader 내 스크롤 애니메이션 적용
    - 스크롤이 100px 넘었을 경우 헤더를 fixed 로 고정 (너비 100%, bg 변경)
    
    메모제이션
    - memo 를 사용하여 navigation 데이터가 변경되지 않으면 리렌더링 방지

--- src/components/Team/TeamHeader.tsx
+++ src/components/Team/TeamHeader.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+'use client';
+import {useState, useEffect, memo} from 'react';
 import NavLink from "@/components/NavLink";
 
 const TeamHeader = ({navigation}) => {
+  const [isFixed, setFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setFixed(true);
+      } else {
+        setFixed(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-      <ul className="w-2/3 border-b-2 border-b-primary flex justify-between text-sm md:text-base">
+      <ul className={`w-2/3 border-b-2 border-b-primary flex transition-all duration-300 text-sm md:text-base ${isFixed ? "fixed top-[72px] left-0 w-full bg-primary text-white flex-start py-2 px-6 shadow-md" : "justify-between"}`}>
         {navigation.map((path, index) => (
             <li key={index} className="w-full text-center md:text-left">
               <NavLink
                   href={path.path}
-                  activeClassName="text-primary border-b-2 border-b-primary md:border-none md:bg-primary md:text-white md:rounded-t-lg"
-                  className="text-center text-sm md:text-lg block w-full py-2 md:p-4 text-[rgba(0,0,0,0.5)] transition-all">
+                  activeClassName={`${
+                      isFixed
+                          ? "border-none rounded-t-lg"
+                          : "text-primary border-b-2 border-b-primary md:border-none md:bg-primary md:text-white md:rounded-t-lg"}`}
+                  className={`text-center text-sm md:text-lg block w-full py-2 md:p-4 transition-all ${isFixed ? "bg-primary text-white" : "text-[rgba(0,0,0,0.5)]"}`}>
                 {path.label}
               </NavLink>
             </li>
@@ -18,4 +37,4 @@ const TeamHeader = ({navigation}) => {
   );
 };
 
-export default TeamHeader;
+export default memo(TeamHeader);
