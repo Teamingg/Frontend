54377c2ff0831783ed50999a1f9842a272ba23d2
Ajunmiiin <wjdals3249@naver.com>
2025-03-04 22:45:54 +0900

    fix : import 구문오류 수정

--- src/service/api/my/getMyMentoringTeam.ts
+++ src/service/api/my/getMyMentoringTeam.ts
@@ -1,12 +1,12 @@
 "use server";
 
-import MentoringTeam from "@/types/team/mentoringTeam";
-import {createServerInstance} from "../instance/server";
+import { MentoringTeam } from "@/types/user/myTeamRes";
+import { createServerInstance } from "../instance/server";
 
 export const getMyMentoringTeam = async (): Promise<MentoringTeam[]> => {
   const server = await createServerInstance();
   const { data } = await server.get("/users/1/mentoring/teams");
-  console.log('getMyMentoringTeam')
-  console.log(data)
+  console.log("getMyMentoringTeam");
+  console.log(data);
   return data.data;
 };
--- src/service/api/my/getMyProjectTeam.ts
+++ src/service/api/my/getMyProjectTeam.ts
@@ -1,12 +1,12 @@
 "use server";
-import ProjectTeam from "@/types/team/projectTeam";
-import {createServerInstance} from "../instance/server";
+import { ProjectTeam } from "@/types/user/myTeamRes";
+import { createServerInstance } from "../instance/server";
 
 const getMyProjectTeam = async (): Promise<ProjectTeam[]> => {
   const server = await createServerInstance();
   const { data } = await server.get("/users/1/project");
-  console.log('getMyProjectTeam')
-  console.log(data)
+  console.log("getMyProjectTeam");
+  console.log(data);
   return data.data;
 };
 
