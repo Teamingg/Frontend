99959aea50ad11c8134488463529d52265418198
Ajunmiiin <wjdals3249@naver.com>
2025-03-04 22:32:47 +0900

    fix : params Nextjs15 문법에 맞게 수정

--- src/app/my/[type]/page.tsx
+++ src/app/my/[type]/page.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import {queryclient} from "@/lib/getQueryClient";
-import {myPageKeys} from "@/hooks/queries/my";
-import {getMyMentoringTeam, getMyProjectTeam} from "@/service/api/my";
-import {TeamSection} from "@/layout/my/TeamSection";
+import React from "react";
+import { queryclient } from "@/lib/getQueryClient";
+import { myPageKeys } from "@/hooks/queries/my";
+import { getMyMentoringTeam, getMyProjectTeam } from "@/service/api/my";
+import { TeamSection } from "@/layout/my/TeamSection";
 import { TeamCard } from "@/components/Team";
 
 const Page = async ({
-  params
+  params,
+}: {
+  params: Promise<{ type: "project" | "mentoring" }>;
 }) => {
-  const {type} = params;
+  const { type } = await params;
 
   const project = await queryclient.fetchQuery({
     queryKey: myPageKeys.team("project"),
@@ -20,47 +22,51 @@ const Page = async ({
     queryFn: getMyMentoringTeam,
   });
 
-  const pageType = type === 'project' ? '프로젝트' : '멘토링';
-  const data = type === 'project' ? project : mentoring;
-  console.log('data');
+  const pageType = type === "project" ? "프로젝트" : "멘토링";
+  const data = type === "project" ? project : mentoring;
+  console.log("data");
   console.log(data);
 
   return (
-      <>
-        {/* Todo 탭 네비게이션을 생성하여 선택한 카테고리 별로 출력 예정 */}
-        <TeamSection
+    <>
+      {/* Todo 탭 네비게이션을 생성하여 선택한 카테고리 별로 출력 예정 */}
+      <TeamSection
         title={`진행중인 ${pageType}`}
         isEmpty={!data || data.length === 0}
-        pageType={type}>
-          {data.map((item, index) => (
-              <TeamCard
-              key={index}
-              title={item.name}
-              teamId={item.id}
-              status={item.status}
-              start={item.startDate}
-              end={item.endDate}
-              progress={item.progress ?? 0}
-              href={`/team/${type}/${item.id}/info`}/>
-          ))}
-        </TeamSection>
-        <TeamSection
+        pageType={type}
+      >
+        {data.map((item, index) => (
+          <TeamCard
+            key={index}
+            title={item.name}
+            teamId={item.id}
+            status={item.status}
+            start={item.startDate}
+            end={item.endDate}
+            progress={item.progress ?? 0}
+            href={`/team/${type}/${item.id}/info`}
+          />
+        ))}
+      </TeamSection>
+      <TeamSection
         title={`종료된 ${pageType}`}
         isEmpty={!data || data.length === 0}
-        pageType={type}>
-          {data.map((item, index) => (
-              <TeamCard
-              key={index}
-              title={item.name}
-              teamId={item.id}
-              status={item.status}
-              start={item.startDate}
-              end={item.endDate}
-              progress={item.progress ?? 0}
-              href={`/team/${type}/${item.id}/info`}/>
-          ))}
-        </TeamSection>
-      </>
+        pageType={type}
+      >
+        {data.map((item, index) => (
+          <TeamCard
+            key={index}
+            title={item.name}
+            teamId={item.id}
+            status={item.status}
+            start={item.startDate}
+            end={item.endDate}
+            progress={item.progress ?? 0}
+            href={`/team/${type}/${item.id}/info`}
+          />
+        ))}
+      </TeamSection>
+    </>
   );
 };
 
