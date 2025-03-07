8709bc71f326ae40029fe712ef89aa05ca309e15
JangHwan <wkd5878@gmail.com>
2025-03-07 17:45:35 +0900

    :bug: fix - 마이페이지 내 프로젝트, 멘토링 페이지 접근시 오류 픽스

--- src/app/team/[type]/[id]/layout.tsx
+++ src/app/team/[type]/[id]/layout.tsx
@@ -12,19 +12,17 @@ const Layout = async ({
   params: Promise<{ type: string; id: string; }>;
 }) => {
   const {type, id} = await params;
-  let team = [];
-  
-  if (type === 'project') {
-    team = await queryclient.fetchQuery({
+  /*if (type === 'project') {
+    await queryclient.fetchQuery({
       queryKey: ["MentoringTeam", id],
       queryFn: () => getServerMentoringTeam(id),
     });
   } else if (type === 'team') {
-    team = await queryclient.fetchQuery({
+    await queryclient.fetchQuery({
       queryKey: ["ProjectTeam", id],
       queryFn: () => getServerProjectInfo(id),
     });
-  }
+  }*/
   
   const navItems = [
     {label: '대시보드', path: `/team/${type}/${id}/dashboard`},
@@ -32,11 +30,10 @@ const Layout = async ({
     {label: '게시글', path: `/team/${type}/${id}/post`},
   ];
   console.log('팀페이지 최상위 레이아웃 실행 로그')
-  
   return (
     <HydrationBoundary state={dehydrate(queryclient)}>
-      <TeamContainer team={team} navItems={navItems} type={type} id={id}>
-          {children}
+      <TeamContainer navItems={navItems}>
+        {children}
       </TeamContainer>
     </HydrationBoundary>
   );
--- src/layout/Team/TeamContainer.tsx
+++ src/layout/Team/TeamContainer.tsx
@@ -1,41 +1,56 @@
 'use client';
 import React, {useEffect} from 'react';
 import TeamHeader from "@/components/Team/TeamHeader";
-import {useRouter} from "next/navigation";
+import {useParams, useRouter} from "next/navigation";
 import LoadingSpinner from "@/components/loading/LoadingSpinner";
+import {useQuery} from "@tanstack/react-query";
+import {
+  getProjectInfo,
+  getTeamInfo
+} from "@/service/api/team/team";
 
 const TeamContainer = ({
   children,
   navItems,
-  team,
-  type,
-  id,
 } : {
   children: React.ReactNode;
   navItems: {label: string; path: string}[];
-  team: any;
-  type: string;
-  id: string;
 }) => {
+  const params = useParams();
   const router = useRouter();
-  const [isLoading, setIsLoading] = React.useState(false);
+  const { type, id } = params;
+ 
+  const { data: mentoring, isLoading: mentoringLoading, error: mentoringErr } = useQuery({
+    queryKey: ["MentoringTeam", id],
+    queryFn: () => getTeamInfo(id as string),
+    staleTime: 1000 * 60 * 5, // 5분 동안 캐싱 유지
+    enabled: type === 'mentoring',
+  });
+  
+  const { data: project, isLoading: projectLoading, error: projectErr } = useQuery({
+    queryKey: ["ProjectTeam", id],
+    queryFn: () => getProjectInfo(id as string),
+    staleTime: 1000 * 60 * 5, // 5분 동안 캐싱 유지
+    enabled: type === 'project',
+  });
   
   useEffect(() => {
-    if (!team) {
-      setIsLoading(true);
-      return;
-    }
-    
-    const isTeamMember = team.role === "NoAuth" || team.userRole === "VISITOR"
-    if (!isTeamMember) router.replace(`/team/${type}/${id}/viewer`);
-    else setIsLoading(false);
-  }, [team, router, type, id])
-  if (isLoading) return <LoadingSpinner/>
+    if (!project || !mentoring) return;
+    const isNotTeamMember = mentoring.role === "NoAuth" || project.userRole === "VISITOR";
+    if (isNotTeamMember) router.replace(`/team/${type}/${id}/viewer`);
+  }, [mentoring, project, router, type, id]);
+  
+  if (mentoringLoading || projectLoading) return <LoadingSpinner/>
+  if (mentoringErr || projectErr) return <LoadingSpinner/>
   console.log('TeamContainer 컴포넌트 실행됨')
+  console.log(mentoring);
+  console.log(project);
+  
+  const dataRole = type === 'mentoring' ? mentoring.role : project.userRole;
   
   return (
     <div className='w-full min-h-[calc(100vh-72px-62px)] py-10 flex flex-col items-center max-w-sm md:max-w-3xl lg:max-w-5xl xl:max-w-7xl mx-auto md:mx-auto'>
-      {team.role === 'NoAuth' || team.userRole === 'VISITOR' && <TeamHeader navigation={navItems}/>}
+      {dataRole.role !== "NoAuth" && dataRole.userRole !== "VISITOR" && <TeamHeader navigation={navItems}/>}
       <section className="w-2/3 h-full min-h-full p-6 mx-auto">
         {children}
       </section>
