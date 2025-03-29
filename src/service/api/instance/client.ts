194c33f143098c4acc08f29a200851eaf59e3425
Ajunmiiin <wjdals3249@naver.com>
2025-03-29 21:10:31 +0900

    fix : 리프레시 토큰이 존재하지 않을 때에도  오류 발생 시 refrershToken 요청 무한루프 이슈 픽스

--- src/service/api/instance/client.ts
+++ src/service/api/instance/client.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 import handleError from "@/service/handleError";
 import { refreshToken } from "../refreshToken";
+import { getCookie } from "@/utils/cookies";
 
 export const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -18,11 +19,13 @@ client.interceptors.response.use(
 
   // 응답에 실패했을 때
   async (error) => {
-    console.log(error.response.data);
+    const token = await getCookie("refresehToken");
+
+    console.log(token);
     // 원래 응답 객체
     const originalRequest = error.config;
     // 액세스 토큰이 만료되었을 때 (401)
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response.status === 401 && token && !originalRequest._retry) {
       // retry 무한루프 방지
       originalRequest._retry = true;
 
@@ -32,7 +35,7 @@ client.interceptors.response.use(
         // refreshToken() 로 액세스토큰 재발급 후 원래 요청 다시시도
         return client(originalRequest);
       } catch {
-        handleError(error.response.status);
+        return handleError(error.response.status);
       }
     }
 
