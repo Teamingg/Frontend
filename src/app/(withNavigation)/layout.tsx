8a899f4d80da86cb2e8038d4fe4e5fa91fca564b
Ajungmiiin <wjdals3249@naver.com>
2025-06-25 21:07:52 +0900

    design : section 요소 스타일 제거

--- src/app/(withNavigation)/layout.tsx
+++ src/app/(withNavigation)/layout.tsx
@@ -2,7 +2,7 @@ import LocalNavigation from "@/layout/Navigation/LocalNavigation";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="pt-[60px] md:pt-[75px]">
+    <section>
       <LocalNavigation />
       <div className="p-4 md:p-8 bg-[#f5f5f5] md:max-w-[1400px] md:mx-auto md:rounded-2xl">
         {children}
