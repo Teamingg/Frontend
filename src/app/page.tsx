67a913240961a52d85ed04b2d09a5d5791be46fd
Ajungmiiin <wjdals3249@naver.com>
2025-06-25 21:07:07 +0900

    design  : section 요소 스타일 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default async function Home() {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <section className="pt-[60px] md:pt-[75px]">
+      <section>
         <LocalNavigation />
         <div className="p-4 md:p-8 bg-[#f5f5f5] md:max-w-[1400px] md:mx-auto md:rounded-2xl">
           <article className="w-full flex flex-col gap-4 md:gap-2">
